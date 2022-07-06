e01bd2d52faed2f1c38bef82e0263e8acd04ea00
hg-tc <1344405655@qq.com>
2022-07-05 22:37:32 -0700

    trainable

--- top4/top4.py
+++ top4/top4.py
@@ -26,3 +26,56 @@ validation_cats_dir = os.path.join(validation_dir, 'cats')
 
 # Directory with our validation dog pictures
 validation_dogs_dir = os.path.join(validation_dir, 'dogs')
+
+
+# Set up matplotlib fig, and size it to fit 4x4 pics
+import matplotlib.image as mpimg
+nrows = 4
+ncols = 4
+
+fig = plt.gcf()
+fig.set_size_inches(ncols*4, nrows*4)
+pic_index = 100
+train_cat_fnames = os.listdir( train_cats_dir )
+train_dog_fnames = os.listdir( train_dogs_dir )
+
+
+next_cat_pix = [os.path.join(train_cats_dir, fname) 
+                for fname in train_cat_fnames[ pic_index-8:pic_index] 
+               ]
+
+next_dog_pix = [os.path.join(train_dogs_dir, fname) 
+                for fname in train_dog_fnames[ pic_index-8:pic_index]
+               ]
+
+for i, img_path in enumerate(next_cat_pix+next_dog_pix):
+  # Set up subplot; subplot indices start at 1
+  sp = plt.subplot(nrows, ncols, i + 1)
+  sp.axis('Off') # Don't show axes (or gridlines)
+
+  img = mpimg.imread(img_path)
+  plt.imshow(img)
+
+plt.show()
+
+
+# Add our data-augmentation parameters to ImageDataGenerator
+train_datagen = ImageDataGenerator(rescale = 1./255.,rotation_range = 40, width_shift_range = 0.2, height_shift_range = 0.2, shear_range = 0.2, zoom_range = 0.2, horizontal_flip = True)
+
+# Note that the validation data should not be augmented!
+test_datagen = ImageDataGenerator( rescale = 1.0/255. )
+
+# Flow training images in batches of 20 using train_datagen generator
+train_generator = train_datagen.flow_from_directory(train_dir, batch_size = 20, class_mode = 'binary', target_size = (224, 224))
+
+# Flow validation images in batches of 20 using test_datagen generator
+validation_generator = test_datagen.flow_from_directory( validation_dir,  batch_size = 20, class_mode = 'binary', target_size = (224, 224))
+
+from tensorflow.keras.applications.vgg16 import VGG16
+
+base_model = VGG16(input_shape = (224, 224, 3), # Shape of our images
+include_top = False, # Leave out the last fully connected layer
+weights = 'imagenet')
+
+for layer in base_model.layers:
+    layer.trainable = False
