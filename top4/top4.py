5e35f86180698fab2f52be1e056c0ab8b9e112fb
hg-tc <1344405655@qq.com>
2022-07-05 22:57:04 -0700

    try different settings

--- top4/top4.py
+++ top4/top4.py
@@ -79,3 +79,19 @@ weights = 'imagenet')
 
 for layer in base_model.layers:
     layer.trainable = False
+
+# Flatten the output layer to 1 dimension
+x = layers.Flatten()(base_model.output)
+
+# Add a fully connected layer with 512 hidden units and ReLU activation
+x = layers.Dense(512, activation='relu')(x)
+
+# Add a dropout rate of 0.5
+x = layers.Dropout(0.5)(x)
+
+# Add a final sigmoid layer with 1 node for classification output
+x = layers.Dense(1, activation='sigmoid')(x)
+
+model = tf.keras.models.Model(base_model.input, x)
+
+model.compile(optimizer = tf.keras.optimizers.RMSprop(lr=0.0001), loss = 'binary_crossentropy',metrics = ['acc'])
